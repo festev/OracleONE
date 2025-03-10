7439e7acbb14769a6827bcc01fdf7af7bd7968b5
festev <federicoestevez3025@gmail.com>
2025-03-09 21:06:44 -0300

    completa tarjeta 'Implementa una función para actualizar la lista de amigos'

--- Challenges/Amigo Secreto/app.js
+++ Challenges/Amigo Secreto/app.js
@@ -3,14 +3,20 @@ let amigos = [];
 function agregarAmigo(){
     let campoTexto = document.querySelector("#amigo");
     let listaAmigos = document.querySelector("#listaAmigos");
+    let bttnDraw = document.querySelector(".button-draw");
 
     if (campoTexto.value == ""){
         alert("ERROR: No hay ningún nombre ingresado. Por favor, inserte un nombre.")
     }
     else{
         amigos.push(campoTexto.value);
+
+        if(amigos.length == 2){
+            bttnDraw.disabled = false;
+        }
+
+        listaAmigos.style.display = "block";
+        listaAmigos.innerHTML += `<li>${amigos[amigos.length-1]}</li>`;
         campoTexto.value = "";
     }
-
-    //listaAmigos.
 }
